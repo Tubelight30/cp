23a6ea7285a47e812ab786ec0a36c4c2e2864aeb
Tubelight30 <soumaygupta2004@gmail.com>
2025-02-07 10:25:31 +0530

    1 cses prob

---
+++ Missing_Coin_Sum.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    unordered_map<int,int> mp;
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+        mp[v[i]]++;
+        sum += v[i];
+    }
+    sort(v.begin(), v.end());
+    if(v[0] != 1) {
+        cout<<1<<endl;
+        return;
+    }
+    int prefixsum = 1;
+    for(int i = 1;i<v.size();i++){
+        if(v[i]>prefixsum+1){
+            cout<<prefixsum+1<<endl;
+            return;
+        }
+        prefixsum+=v[i];
+    }
+    cout<<prefixsum+1<<endl;
+}
+
+int32_t main()
+{
+    solve();
+    return 0;
+}
